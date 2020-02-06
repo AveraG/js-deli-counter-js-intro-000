69ab68a8a9cab2b9aba0f5646d34a4889f1623b7
Avera Guthrie <averaguthrie@gmail.com>
2020-02-06 17:19:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function nowServing(katzDeliLine) {
 };
 
 function currentLine(current) {
-  var line
   if(current.length === 0) {
     return 'The line is currently empty.';
   } else {
+    var line
     for(let i = 0; i < current.length; i++) {
       line.push(i+1 + '. ' + line[i])
     }
