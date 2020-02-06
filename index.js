874d06c0ca685a1fd336a124f6c2a8b48085f67c
Avera Guthrie <averaguthrie@gmail.com>
2020-02-06 16:54:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-function takeANumber(current, newPerson) {
-  var place = current.length + 1;
+function takeANumber(katzDeliLine, newPerson) {
+  var place = katzDeliLine.length + 1;
   var greeting = 'Welcome, ' + newPerson + '. You are number ' + place + ' in line.';
   current.push(newPerson);
   return greeting;
+};
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length > 0) {
+    return katzDeliLine[1];
+    katzDeliLine.shift();
+  } else {
+    return 'There is nobody waiting to be served!'
+  }
 }
